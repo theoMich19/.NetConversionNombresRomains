79906232e875a046da8de76fb0080fc123dbbee0
theoMich <michellontheo.pro@gmail.com>
2025-01-22 10:56:15 +0100

    feat(global): v2 all

--- ConversionNombresRomains/obj/Debug/net9.0/ConversionNombresRomains.AssemblyInfo.cs
+++ ConversionNombresRomains/obj/Debug/net9.0/ConversionNombresRomains.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConversionNombresRomains")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14294f0937948b5e054dc94ada9c32575ded5f14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad80ea8d84ce82eed2b6304ea339a09866a3727")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConversionNombresRomains")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConversionNombresRomains")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
